7a702a8616a60663b659e590786f50b8322ee51a
svancise <slvancise@gmail.com>
2016-02-07 18:52:18 -0500

    basic team page place holder until we have stats

--- fls_frontend/playbyplay/views.py
+++ fls_frontend/playbyplay/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def games(request):
+    context = {'active_page' : 'games'}
+    return render(request, 'playbyplay/games.html', context)
--- fls_frontend/website/views.py
+++ fls_frontend/website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from playbyplay.models import Game
 from team.models import Team
 import datetime
@@ -10,7 +9,14 @@ def index(request):
     games = Game.objects.filter(dateTime__date__lte=datetime.date.today()).order_by('-dateTime', '-gamePk')[:15]
     teams = Team.objects.all()
     context = {
+        'active_page': 'index',
         'game_list': games,
         'teams': teams
     }
     return render(request, 'website/index.html', context)
+
+def about(request):
+    context = {
+        'active_page': 'about'
+    }
+    return render(request, 'website/about.html', context)
