485fc4293cb5ee736fcd4bf1e5ba9cb6ed99eff9
Spencer Van Cise <slvancise@gmail.com>
2016-02-09 19:11:02 -0500

    re-add images

--- fls_frontend/playbyplay/views.py
+++ fls_frontend/playbyplay/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
-from playbyplay.models import Game, PlayByPlay
+from playbyplay.models import Game, PlayByPlay, PlayerGameStats, GoalieGameStats
 from team.models import Team
 import datetime
 
@@ -20,49 +20,67 @@ def games(request):
 def game_page(request, game_pk):
     try:
         game = Game.objects.get(gamePk = game_pk)
-        pbp = PlayByPlay.objects.filter(gamePk=game_pk)
-
-        #PN
-        game.awayPN = 0
-        game.homePN = 0
-        for play in pbp:
-            if play.team == game.homeTeam and play.playType == 'PENALTY':
-                game.homePN += 1
-            elif play.team == game.awayTeam and play.playType == 'PENALTY':
-                game.awayPN += 1
+        if game.gameState in ['3', '4', '5', '6', '7']:
+            pbp = PlayByPlay.objects.filter(gamePk=game_pk)
+            playerStats = PlayerGameStats.objects.filter(game=game_pk).order_by('team', 'player__lastName')
+            goalieStats = GoalieGameStats.objects.filter(game=game_pk)
 
 
-        #Fenwick (FF)
-        game.awayFF = int(game.awayShots) + int(game.awayMissed)
-        game.homeFF = int(game.homeShots) + int(game.homeMissed)
-        #MSF
-        game.awayMSF = int(game.awayFF) - int(game.awayShots)
-        game.homeMSF = int(game.homeFF) - int(game.homeShots)
-        #Corsi
-        game.awayCorsi = int(game.awayShots) + int(game.homeBlocked) + int(game.awayMissed)
-        game.homeCorsi = int(game.homeShots) + int(game.awayBlocked) + int(game.homeMissed)
-        #BSF
-        game.awayBSF = int(game.homeBlocked)
-        game.homeBSF = int(game.awayBlocked)
-        #SCF
-        game.awaySCF = None
-        game.homeSCF = None
-        #HSCF
-        game.awayHSCF = None
-        game.homeHSCF = None
-        #ZSO
-        game.awayZSO = None
-        game.homeZSO = None
-        #HIT
-        game.awayHIT = int(game.awayHits)
-        game.homeHIT = int(game.homeHits)
-        #FO_W
-        game.awayFO_W = None
-        game.homeFO_W = None
-        #TOI
-        game.awayTOI = None
-        game.homeTOI = None   
+            #Goalie Stats
+            game.goalieStats = goalieStats
 
+            #Player Stats
+            game.homePlayers = []
+            game.awayPlayers = []
+            for player in playerStats:
+                player.points = player.goals + player.assists
+                player.faceOffLost = player.faceoffTaken - player.faceOffWins
+                if player.team == game.homeTeam:
+                    game.homePlayers.append(player)
+                elif player.team == game.awayTeam:
+                    game.awayPlayers.append(player)
+
+
+            #PN
+            game.awayPN = 0
+            game.homePN = 0
+            for play in pbp:
+                if play.team == game.homeTeam and play.playType == 'PENALTY':
+                    game.homePN += 1
+                elif play.team == game.awayTeam and play.playType == 'PENALTY':
+                    game.awayPN += 1
+
+
+            #Fenwick (FF)
+            game.awayFF = game.awayShots + game.awayMissed
+            game.homeFF = game.homeShots + game.homeMissed
+            #MSF
+            game.awayMSF = game.awayFF - game.awayShots
+            game.homeMSF = game.homeFF - game.homeShots
+            #Corsi
+            game.awayCorsi = game.awayShots + game.homeBlocked + game.awayMissed
+            game.homeCorsi = game.homeShots + game.awayBlocked + game.homeMissed
+            #BSF
+            game.awayBSF = game.homeBlocked
+            game.homeBSF = game.awayBlocked
+            #SCF
+            game.awaySCF = None
+            game.homeSCF = None
+            #HSCF
+            game.awayHSCF = None
+            game.homeHSCF = None
+            #ZSO
+            game.awayZSO = None
+            game.homeZSO = None
+            #HIT
+            game.awayHIT = game.awayHits
+            game.homeHIT = game.homeHits
+            #FO_W
+            game.awayFO_W = None
+            game.homeFO_W = None
+            #TOI
+            game.awayTOI = None
+            game.homeTOI = None   
     except Game.DoesNotExist:
         raise Http404("Game does not exist!")
     return render(request, 'playbyplay/game_page.html', {
